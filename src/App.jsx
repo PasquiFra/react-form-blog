c0600b6e8ab09d3c7f3d21711f6970577e16ab5e
Your Name <you@example.com>
2024-06-18 14:40:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from "./layouts/Footer"
+
+function App() {
+  <>
+    <Header />
+    <main>
+
+    </main>
+    <Footer />
+  </>
+}
+
+export default App
